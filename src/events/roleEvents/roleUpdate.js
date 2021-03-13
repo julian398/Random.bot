eacc20f3e56da5c8e7a55cf03cf357b62715e51a
julian398 <julian398a@yahoo.es>
2021-03-13 02:29:52 -0500

    hey vic check this

--- src/events/roleEvents/roleUpdate.js
+++ src/events/roleEvents/roleUpdate.js
@@ -1,4 +1,5 @@
 import { AbstractEvent } from "../../model/AbstractEvent";
+const Discord = require('discord.js');
 
 export class roleUpdate extends AbstractEvent{
 
@@ -12,13 +13,53 @@ export class roleUpdate extends AbstractEvent{
     execute({commandObject}) {
         const [oldRole, newRole] = commandObject;
         const channeltosendid = "818172844277891083";
-        const oldname = oldRole.name;
-        const newname = newRole.name;
-        if(oldname === newname){
-            return;
+        const roleChangeEmbed = new Discord.MessageEmbed()
+        .setColor(oldRole.color)
+        .setTitle(`Role change`)
+        .addFields(
+            {name: `Role name change`, value: `${this.roleNameChange(oldRole,newRole)}`},
+            {name: `Role color change`, value: `${this.roleColorChange(oldRole, newRole)}`}  
+        );
+        if(this.rolePermissionsCheck(oldRole,newRole) === true){
+            roleChangeEmbed.addFields(
+                {name: `Permissions added`, value: `${this.rolePermissionsAdded(oldRole,newRole)}`},
+                {name: `Permissions removed`, value: `${this.rolePermissionsRemoved(oldRole,newRole)}`}
+            )
+        };
+        if(!oldRole.color === newRole.color){
+            roleChangeEmbed.setColor(newRole.color);
         }
         const channelToSend = newRole.guild.channels.cache.get(channeltosendid);
-        channelToSend.send(`Role's old name: ${oldname} \nRole's new name: ${newname}`);
+        channelToSend.send(roleChangeEmbed);
     }
+
+    roleNameChange(oldRole, newRole){
+        if(oldRole.name === newRole.name){
+            return `Role name is sill ${oldRole.name}`;
+        }
+        return (`Role's old name: ${oldRole.name} \nRole's new name: ${newRole.name}`);
+    }
+
+    roleColorChange(oldRole, newRole){
+        if(oldRole.color === newRole.color){
+            return `No color change`;
+        }
+        return (`Role's old color: ${oldRole.color} \nRole's new color: ${newRole.color}`);
+    }
+    rolePermissionsCheck(oldRole, newRole){
+        const permsadd = oldRole.permissions.missing(newRole.permissions.bitfield);
+        const permsremove = newRole.permissions.missing(oldRole.permissions.bitfield);
+        if(permsadd.length === 0 && permsremove.length === 0 ){
+            return false;
+        }
+        return true;
+    }
+    rolePermissionsAdded(oldRole, newRole){
+        return oldRole.permissions.missing(newRole.permissions.bitfield);
+    }
+    rolePermissionsRemoved(oldRole, newRole){
+        return newRole.permissions.missing(oldRole.permissions.bitfield);
+    }
+
 }
 new roleUpdate();
