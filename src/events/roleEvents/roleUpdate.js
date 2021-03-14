1038b128cd4d09a53fc82e1fa6260ab4469c302c
julian398 <julian398a@yahoo.es>
2021-03-13 19:09:20 -0500

    role update

--- src/events/roleEvents/roleUpdate.js
+++ src/events/roleEvents/roleUpdate.js
@@ -15,17 +15,20 @@ export class roleUpdate extends AbstractEvent{
         const channeltosendid = "818172844277891083";
         const roleChangeEmbed = new Discord.MessageEmbed()
         .setColor(oldRole.color)
-        .setTitle(`Role change`)
+        .setTitle(`Role change "${oldRole.name}"`)
         .addFields(
             {name: `Role name change`, value: `${this.roleNameChange(oldRole,newRole)}`},
             {name: `Role color change`, value: `${this.roleColorChange(oldRole, newRole)}`}  
         );
-        if(this.rolePermissionsCheck(oldRole,newRole) === true){
-            roleChangeEmbed.addFields(
-                {name: `Permissions added`, value: `${this.rolePermissionsAdded(oldRole,newRole)}`},
-                {name: `Permissions removed`, value: `${this.rolePermissionsRemoved(oldRole,newRole)}`}
-            )
-        };
+        const {added, removed} = this._getPremissionChanges(oldRole,newRole);
+
+        if(added.length > 0){
+            roleChangeEmbed.addField( "Permissions added", `${added.join(", ")}`);
+        }
+
+        if(removed.length > 0){
+            roleChangeEmbed.addField( "Permissions removed", `${removed.join(", ")}`);
+        }
         if(!oldRole.color === newRole.color){
             roleChangeEmbed.setColor(newRole.color);
         }
@@ -46,20 +49,20 @@ export class roleUpdate extends AbstractEvent{
         }
         return (`Role's old color: ${oldRole.color} \nRole's new color: ${newRole.color}`);
     }
-    rolePermissionsCheck(oldRole, newRole){
+    
+    _getPremissionChanges(oldRole, newRole){
+        const retObj = {
+          added: [],
+          removed: []
+        };
         const permsadd = oldRole.permissions.missing(newRole.permissions.bitfield);
         const permsremove = newRole.permissions.missing(oldRole.permissions.bitfield);
-        if(permsadd.length === 0 && permsremove.length === 0 ){
-            return false;
+        if(permsadd.length > 0 || permsremove.length > 0 ){
+           retObj["added"] = permsadd;
+           retObj["removed"] = permsremove;
         }
-        return true;
-    }
-    rolePermissionsAdded(oldRole, newRole){
-        return oldRole.permissions.missing(newRole.permissions.bitfield);
-    }
-    rolePermissionsRemoved(oldRole, newRole){
-        return newRole.permissions.missing(oldRole.permissions.bitfield);
-    }
+        return retObj;
+      }
 
 }
 new roleUpdate();
